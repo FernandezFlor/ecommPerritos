7d29c05bfc3176c5e8f853dec5fdae718c91ab65
Flor <flomerfer64@gmail.com>
2022-09-23 23:42:37 -0300

    context

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ItemDetailContainer from './components/ItemDetailContainer.js'
 import ItemListContainer from './components/ItemListContainer.js';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Cart from './components/Cart.js'
+import {CartContext} from './components/CartContext.js'
 import './App.css';
 
 
@@ -10,16 +11,18 @@ import './App.css';
 function App() {
   return (
     <>
-    <Router>
-    <Nav />
-    
-    <Routes>
-      <Route exact path='/' element={<ItemListContainer greeting={"Hola Mundo"}/>}/>
-      <Route path='category/:categoria' element={<ItemListContainer greeting={"Hola mundo"}/>}/>
-      <Route path='item/:id' element={<ItemDetailContainer/>}/>
-      <Route path ='item/:id/:cart' element={<Cart/>}/>
-    </Routes>
-    </Router>
+    <CartContext.Provider value={['Coca Cola','Cocucha']}>
+      <Router>
+        <Nav />
+        
+        <Routes>
+          <Route exact path='/' element={<ItemListContainer greeting={"Hola Mundo"}/>}/>
+          <Route path='category/:categoria' element={<ItemListContainer greeting={"Hola mundo"}/>}/>
+          <Route path='item/:id' element={<ItemDetailContainer/>}/>
+          <Route path ='item/:id/:cart' element={<Cart/>}/>
+        </Routes>
+      </Router>
+    </CartContext.Provider>
     </>
   );
 }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,14 @@
+import { useContext } from 'react';
+import {CartContext} from './CartContext.js'
 const Cart = () =>{
+  const ctx=useContext(CartContext);
+ 
   return(
       <>
           <h1>Carrito</h1>
+          {
+            ctx.map(item=><li>{item}</li>)
+          }
       </>
   )
 }
